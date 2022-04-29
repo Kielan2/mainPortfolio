0e782c807c42279d5f405ec22e185dc1a7513149
Kielan2 <kielananderson@gmail.com>
2022-04-29 14:44:36 -0600

    background hide and menu

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ let a
       time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
       document.getElementById('sideNote').innerHTML = time;
     }, 1000)
+
+    document.getElementById('aboutLink').addEventListener('click', showAbout)
+    
+    let aboutSection = document.querySelector('.tvAbout')
+    
+
+    function showAbout(){
+        
+        aboutSection.classList.toggle('hidden')
+    }
