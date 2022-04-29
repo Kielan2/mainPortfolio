dc2bff89cf641fde4f6e448141a8c1606bb1f29b
Kielan2 <kielananderson@gmail.com>
2022-04-29 17:04:24 -0600

    animation, media query, js

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
-let a
-    let time
-    setInterval(() => {
-      a = new Date();
-      time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
-      document.getElementById('sideNote').innerHTML = time;
-    }, 1000)
+let a;
+let time;
+setInterval(() => {
+  a = new Date();
+  time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds();
+  document.getElementById("sideNote").innerHTML = time;
+}, 1000);
 
-    document.getElementById('aboutLink').addEventListener('click', showAbout)
-    
-    let aboutSection = document.querySelector('.tvAbout')
-    
+document.getElementById("aboutLink").addEventListener("click", showAbout);
+document.getElementById("contactLink").addEventListener("click", showContact);
+let aboutSection = document.querySelector(".tvAbout");
+let circle = document.querySelector(".circles");
+let contactSection = document.querySelector(".tvContact");
 
-    function showAbout(){
-        
-        aboutSection.classList.toggle('hidden')
-    }
+function showAbout() {
+  circle.classList.toggle("hidden");
+  aboutSection.classList.toggle("hidden");
+}
+
+function showContact() {
+  circle.classList.toggle("hidden");
+  contactSection.classList.toggle("hidden");
+  aboutSection.classList.toggle("hidden");
+}
+
+
